ff3f50e3797c34fc6c43ccbc8341f8179b95bf3d
kshitiz <karki.kshitiz.23@gmail.com>
2025-06-28 16:43:37 +0545

    version 1.0

--- install/cascadia-mono-nerd-font.sh
+++ install/cascadia-mono-nerd-font.sh
@@ -1,15 +1,26 @@
-if [ "$ID" == "arch" ]; then
-  sudo pacman -Sy --needed --noconfirm ttf-cascadia-mono-nerd
-elif [ "$ID" == "fedora" ]; then
-  mkdir -p ~/.fonts
-  cd ~/Downloads
-  wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
-  unzip CascadiaMono.zip -d CascadiaFont
-  cp -r CascadiaFont ~/.fonts
-  rm -rf CascadiaMono.zip CascadiaFont
-  fc-cache -v
-  cd -
-else
-  printf "%b\n" "${YELLOW}No scripts present for ${ID}, exiting ...${RC}"
-  exit 1
-fi
+# if [ "$ID" == "arch" ]; then
+#   sudo pacman -Sy --needed --noconfirm ttf-cascadia-mono-nerd
+# elif [ "$ID" == "fedora" ]; then
+#   mkdir -p ~/.fonts
+#   cd ~/Downloads
+#   wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+#   unzip CascadiaMono.zip -d CascadiaFont
+#   cp -r CascadiaFont ~/.fonts
+#   rm -rf CascadiaMono.zip CascadiaFont
+#   fc-cache -v
+#   cd -
+# else
+#   printf "%b\n" "${YELLOW}No scripts present for ${ID}, exiting ...${RC}"
+#   exit 1
+# fi
+mkdir -p ~/.fonts
+cd ~/Downloads
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+unzip CascadiaMono.zip -d CascadiaFont
+cp CascadiaFont/CaskaydiaMonoNerdFont-Regular.ttf ~/.fonts
+cp CascadiaFont/CaskaydiaMonoNerdFont-Bold.ttf ~/.fonts
+cp CascadiaFont/CaskaydiaMonoNerdFont-Italic.ttf ~/.fonts
+cp CascadiaFont/CaskaydiaMonoNerdFont-BoldItalic.ttf ~/.fonts
+rm -rf CascadiaMono.zip CascadiaFont
+fc-cache
+cd -
