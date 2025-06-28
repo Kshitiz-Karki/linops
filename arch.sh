525f98e6d4ea34f0db65259098edce30dc31eb04
kshitiz <karki.kshitiz.23@gmail.com>
2025-06-28 11:30:03 +0545

    version 1.0

--- arch.sh
+++ arch.sh
@@ -1,13 +1,14 @@
 set -e
 
-printf "%b\n" "${YELLOW}Running pacman -Syu ...${RC}"
-sudo pacman -Syu
-
 printf "%b\n" "${YELLOW}Enabling bluetooth ...${RC}"
 sudo systemctl enable --now bluetooth
 
 printf "%b\n" "${YELLOW}Installing terminal apps and packages ...${RC}"
-sudo pacman -Sy --needed aria2 stow bat zoxide tldr trash-cli fzf ripgrep fd kitty zsh eza starship wl-clipboard neovim luarocks tree-sitter-cli yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick btop htop
+sudo pacman -Sy --needed aria2 stow bat zoxide tldr trash-cli \
+  fzf ripgrep fd kitty zsh eza starship wl-clipboard \
+  neovim luarocks tree-sitter-cli \
+  yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick btop htop \
+  man less
 
 printf "%b\n" "${YELLOW}Installing yay ...${RC}"
 sudo pacman -Sy --needed base-devel git
---
+++ configure/python-venv.sh
@@ -0,0 +1,8 @@
+#https://developer.fedoraproject.org/tech/languages/python/multiple-pythons.html
+sudo dnf install python3.11
+python3.11 --version
+
+python3.11 -m venv --prompt . .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python3 -m pip install pytest pytest-cache pytest-subtests pytest-pylint
