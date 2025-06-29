87c50bec779079979ac1ce36fa9e26cab7b57bc8
kshitiz <karki.kshitiz.23@gmail.com>
2025-06-29 10:51:12 +0545

    version 1.0

--- install/bibata-cursor-theme.sh
+++ install/bibata-cursor-theme.sh
@@ -2,6 +2,7 @@ cd ~/Downloads
 rm -rf GTK-themes-icons
 git clone https://github.com/JaKooLit/GTK-themes-icons.git --depth 1
 mkdir -p ~/.icons
+rm -rf ~/.icons/Bibata-Modern-Ice
 unzip -o -q "GTK-themes-icons/icon/Bibata-Modern-Ice.zip" -d ~/.icons/
 gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
 rm -rf GTK-themes-icons
---
+++ install/hyprland.sh
@@ -0,0 +1,4 @@
+#prerequisite
+sudo pacman -Sy --needed --noconfirm kitty hyprland \
+  wofi waybar ttf-font-awesome hyprshot mako \
+  hyprlock hypridle
