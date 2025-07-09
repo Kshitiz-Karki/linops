4a4d30ee25ac0b385a0a2e378050b3d4e730578f
kshitiz <karki.kshitiz.23@gmail.com>
2025-07-09 21:25:55 +0545

    simplify and update

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,1 +1,1 @@
-wget -qO- https://raw.githubusercontent.com/Kshitiz-Karki/omarchy/refs/heads/hyprbase/boot.sh | bash
+wget -qO- https://raw.githubusercontent.com/Kshitiz-Karki/omarchy/refs/heads/master/boot.sh | bash
