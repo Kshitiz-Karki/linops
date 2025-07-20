1f0611477bc529b69c344888aabbfe3088dbad63
kshitiz <karki.kshitiz.23@gmail.com>
2025-07-20 16:05:17 +0545

    add hyprland install command

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,1 +1,10 @@
 wget -qO- https://raw.githubusercontent.com/Kshitiz-Karki/omarchy/refs/heads/master/boot.sh | bash
+
+sudo pacman -S --noconfirm --needed hyprland wofi waybar ttf-font-awesome
+
+
+
+windowrulev2 = workspace 10, class:^(obs)$
+windowrulev2 = focus, class:^(obs)$
+
+hyprctl reload
