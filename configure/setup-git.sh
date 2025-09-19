e7057b08fbdb2ca79a3987178a41d9a37c9d430b
kshitiz <karki.kshitiz.23@gmail.com>
2025-09-19 12:33:14 +0545

    add latest changes

--- configure/setup-git.sh
+++ configure/setup-git.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-RC='\033[0m'
-YELLOW='\033[33m'
-GREEN='\033[32m'
-
 git config --global user.name "kshitiz"
 git config --global user.email "karki.kshitiz.23@gmail.com"
 git config --global init.defaultBranch main
-printf "%b\n" "${YELLOW}Run below commands, upload the generated key to github!${RC}"
-printf "%b\n" "${GREEN}ssh-keygen -t ed25519 -C "karki.kshitiz.23@gmail.com"${RC}"
-printf "%b\n" "cat ~/.ssh/id_ed25519.pub${RC}"
+#Run below commands, upload the generated key to github!
+ssh-keygen -t ed25519 -C "karki.kshitiz.23@gmail.com"${RC}"
+cat ~/.ssh/id_ed25519.pub${RC}"
---
+++ fedora/initial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#initial setup
+sudo dnf update -y
+#https://tuxdigital.com/videos/9-things-to-do-after-installing-fedora-42-post-setup-guide/
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install libavcodec-freeworld
+#backup
+sudo dnf install timeshift -y
