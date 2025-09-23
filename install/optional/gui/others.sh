91a0cbde59b65bc2fd8619c413adc4abc94c1c0d
kshitiz <karki.kshitiz.23@gmail.com>
2025-09-23 09:31:03 +0545

    add for proton vpn

--- install/optional/gui/others.sh
+++ install/optional/gui/others.sh
@@ -31,3 +31,11 @@ sudo pacman -Rns spotify-launcher
 #vscode
 sudo pacman -S --needed code
 sudo pacman -Rns code
+
+#proton vpn fedora - install
+wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
+sudo dnf install ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update --refresh
+sudo dnf install proton-vpn-gnome-desktop
+
+#proton vpn fedora - uninstall
+sudo dnf remove proton-vpn-gnome-desktop protonvpn-stable-release
