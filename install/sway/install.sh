b692c70e4fc41d472aed61bcb9b50267279542a1
kshitiz <karki.kshitiz.23@gmail.com>
2025-12-28 19:21:53 +0545

    add latest - dec 28

--- install/sway/install.sh
+++ install/sway/install.sh
@@ -1,4 +1,14 @@
 sudo dnf install @sway-desktop-environment
 
 sudo dnf remove dunst
-sudo dnf install grimshot mako
+
+sudo dnf install wlsunset swaync grimshot pavucontrol
+
+sudo dnf copr enable alternateved/cliphist
+sudo dnf install cliphist
+
+sudo dnf install blueman-manager / sudo dnf install blueman
+blueman-manager
+
+# https://flathub.org/en/apps/com.saivert.pwvucontrol
+flatpak install flathub com.saivert.pwvucontrol
