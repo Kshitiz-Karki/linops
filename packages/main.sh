0b728d5be107286ce22607e8123e444adba98504
kshitiz <karki.kshitiz.23@gmail.com>
2026-07-04 22:39:04 +0545

    sanity test 13

--- packages/main.sh
+++ packages/main.sh
@@ -80,11 +80,11 @@ sudo dnf install -y \
 # waypaper
 #brave browser
 sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 # sudo dnf install -y brave-browser
-sudo dnf install -y brave-origin
+# sudo dnf install -y brave-origin
 # zed editor
-curl -f https://zed.dev/install.sh | sh
+# curl -f https://zed.dev/install.sh | sh
 
 # install terra respository
 sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
